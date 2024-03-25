e0c759d3bfb90436547074c3b4c0b0f6b2e7de96
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2024-03-25 15:22:34 +0000

    Update 404.js, Add rules/page.tsx
    Fix problem with 404 page. Unescaped '
    
    Added a base for the rules page. Is incomplete. May implement multiple pages

---
+++ pages/rules/page.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import s from '../Home.module.css'
+
+const Page: NextPage = () => {
+    return (
+        <div className={s.container}>
+            <main className={s.main}>
+                
+                <h1 className={s.title}>Rules</h1>
+                <p className={s.description}>Looks like I haven&apos;t finished this page! Come back later to see the additions to this page</p>
+                
+            </main>
+        </div>
+    );
+};
+
+export default Page;
