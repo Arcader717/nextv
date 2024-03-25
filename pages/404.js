aa3bb41fe3075f41f6516e251651b0ef762bebbf
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2024-03-25 14:51:11 +0000

    Add description and update the title styles

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,8 @@ import style1 from '../styles/Home.module.css'
 export default function Custom404() {
   return (
     <div className={style1.container}>
-      <h1 className={styles.main}>404</h1>
+      <h1 className={style1.title}>404</h1>
+      <p className={style1.description}>You moron</p>
     </div>
   )
 }
