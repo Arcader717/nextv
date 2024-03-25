f4089dfa4ad25732edfc721ae17d4c2dc348d9ba
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2024-03-25 15:59:34 +0000

    Add rules CSS, update /rules
    Add rules CSS, for the /rules page. Added import for rules CSS in /rules

--- pages/rules.tsx
+++ pages/rules.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import s from '../styles/Home.module.css'
+import s1 from '../styles/rules.module.css'
 
 const Page: NextPage = () => {
     return (
         <div className={s.container}>
             <p className={s.backarr}>&larr;</p>
             
-            <main className={s.main}>
+            <main className={s1.main}>
                 
                 <h1 className={s.title}>Rules</h1>
                 <p className={s.description}>Looks like I haven&apos;t finished this page! Come back later to see the additions to this page</p>
