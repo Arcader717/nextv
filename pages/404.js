808419ba892f9d781350d91338d30e0d2de2deb8
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2024-03-25 14:49:04 +0000

    Update 404 and it's styles

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,10 @@
 import styles from '../styles/404.module.css'
+import style1 from '../styles/Home.module.css'
 
 export default function Custom404() {
-  return <h1 className={styles.main}>404 - Page Not Found</h1>
+  return (
+    <div className={style1.container}>
+      <h1 className={styles.main}>404</h1>
+    </div>
+  )
 }
